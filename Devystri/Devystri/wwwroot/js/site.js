795a03a0082667f5f166367428a5907a6e452700
Alexys-Devystri <66492231+Alexys-Devystri@users.noreply.github.com>
2020-12-31 11:54:02 +0100

    Correction bug main page

--- Devystri/Devystri/wwwroot/js/site.js
+++ Devystri/Devystri/wwwroot/js/site.js
@@ -23,4 +23,15 @@ $(window).resize(function () {
 });
 
 
+// Scroll main page
+
+$(function () {
+    $(".down-arrow").on("click", function (event) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({ scrollTop: $(hash).offset().top }, 900, function () { window.location.hash = hash; })
+    });
+
+})
+
 
