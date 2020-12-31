8bfb309fe3c20eda94b8c810d086cd5028de1538
DimitriTimoz <dimitri.timoz@orange.fr>
2020-12-31 19:06:08 +0100

    compress and db prepare

--- Devystri/Devystri/Startup.cs
+++ Devystri/Devystri/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.StaticFiles;
 
 namespace Devystri
 {
@@ -26,6 +21,7 @@ namespace Devystri
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
