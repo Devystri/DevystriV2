27cbb743115bb6d2944cb6736d6ab0be8b09f9ce
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-01 23:03:48 +0100

    websites backend user

--- Devystri/Devystri/Pages/Web-Sites/List.cshtml.cs
+++ Devystri/Devystri/Pages/Web-Sites/List.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +11,28 @@ namespace Devystri.Pages.Web_Sites
 {
     public class ListModel : PageModel
     {
+        private MyDbContext dbContext;
+        public List<WebSites> webSites;
+        public ListModel(MyDbContext context)
+        {
+            dbContext = context;
+            foreach (var item in dbContext.WebSites.ToList())
+            {
+                dbContext.WebSites.Remove(item);
+            }
+            context.Add(new WebSites()
+            {
+                Name = "What Is It ?",
+                AppLogoName = "Logo-whatIsIt.svg",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis lorem odio, et vestibulum mi bibendum in. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean sodales nibh vel felis hendrerit, ac luctus felis accumsan. Vivamus vitae elit commodo, lacinia elit ut, porttitor felis. In magna lorem, malesuada ut vehicula vel, gravida ac mauris.",
+                Link = "http://what-is-it.fr/",
+                PresentationRessource = "WhatIsItMacbook.png",
+                Stat = (int)Stats.CANCEL
+            });
+            context.SaveChanges();
+            webSites = context.WebSites.ToList();
+
+        }
         public void OnGet()
         {
         }
