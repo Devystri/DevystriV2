399300f815501b35be023895a99bc58f67443236
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-02 15:31:26 +0100

    backend on client finished PMV

--- Devystri/Devystri/Program.cs
+++ Devystri/Devystri/Program.cs
@@ -1,5 +1,8 @@
+using Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,24 @@ namespace Devystri
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+                try
+                {
+                    db.Database.Migrate();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
