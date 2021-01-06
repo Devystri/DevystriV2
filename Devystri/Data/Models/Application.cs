6c55f06dd8fe52181b48b9144cc8d34f29adc0b3
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-06 17:05:31 +0100

    filter

--- Devystri/Data/Models/Application.cs
+++ Devystri/Data/Models/Application.cs
@@ -10,25 +10,25 @@ namespace Data.Models
         [Required]
         public int Id { get; set; }
 
-        [MinLength(4)]
-        [DataType(DataType.Text)]
-        [Required]
+        [MinLength(3, ErrorMessage = "Veuillez spécifier un nom d'au moins 3 caractères.")]
+        [DataType(DataType.Text, ErrorMessage = "La saisie est invalide.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Veuillez spécifier un nom.")]
         public string Name { get; set; }
 
-        [MinLength(12)]
-        [Required]
+        [MinLength(12, ErrorMessage = "La description doit au moins contenir 12 caractères.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez spécifier une desciption.")]
         [DataType(DataType.Text)]
         public string Description { get; set; }
         public int IsOnAppStore { get; set; }
         public int IsOnPlayStore { get; set; }
        
-        [DataType(DataType.ImageUrl)]
+        [DataType(DataType.ImageUrl, ErrorMessage = "Vous n'avez pas saisi un nom d'image correct.")]
         [Required]
         public string AppLogoName { get; set; }
         
         [MinLength(12)]
-        [DataType(DataType.ImageUrl)]
-        [Required]
+        [DataType(DataType.ImageUrl, ErrorMessage = "Vous n'avez pas saisi un nom d'image correct.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vous n'avez pas saisi un nom de ressource correct")]
         public string PresentationRessource { get; set; }
         
         public int Stat { get; set; }
--- Devystri/Data/Models/IoT.cs
+++ Devystri/Data/Models/IoT.cs
@@ -11,12 +11,18 @@ namespace Data.Models
         public int Id { get; set; }
 
         [DataType(DataType.Text)]
+        [MinLength(3, ErrorMessageResourceName = "Le nom du fichier doit au moins contenir 3 caractères.")]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Veuillez saisir le nom du projet.")]
         public string Name { get; set; }
 
         [DataType(DataType.Text)]
+        [MinLength(12, ErrorMessageResourceName = "La description doit contenir au minimum 12 caractères.")]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Veuillez saisir une description.")]
         public string Description { get; set; }
         public float Price { get; set; }
-        [DataType(DataType.ImageUrl)]
+
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "Veuillez saisir le nom d'une ressource de présentation.")]
+        [DataType(DataType.ImageUrl, ErrorMessageResourceName = "Veuillez spécifier une ressource valide.")]
         public string PresentationRessource { get; set; }
         public int Stat { get; set; }
     } 
