2ddd0ab7f90ecef98889a418782b4de94c71e270
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-11 18:12:06 +0100

    add app authorize

--- Devystri/Devystri/Pages/Admin/AddApplication.cshtml.cs
+++ Devystri/Devystri/Pages/Admin/AddApplication.cshtml.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Devystri.Pages.Admin
 {
+#if REALESE
+    [Authorize]
+#endif
     public class AddApplicationModel : PageModel
     {
         public void OnGet()
