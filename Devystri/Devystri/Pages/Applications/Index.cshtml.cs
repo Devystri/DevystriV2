b1d33841a96b09db962b46212daaa95e1da9f3be
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-15 16:29:12 +0100

    push because I must

--- Devystri/Devystri/Pages/Applications/Index.cshtml.cs
+++ Devystri/Devystri/Pages/Applications/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,30 @@ namespace Devystri.Pages.Applications
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Application Application { get; set; }
+        private MyDbContext dbContext;
+        public IndexModel(MyDbContext context)
         {
+            dbContext = context;
+        }
+        public void OnGet(string appName)
+        {
+            if(appName == String.Empty)
+            {
+                return;
+            }
+            else
+            {
+                appName = appName.ToLower();
+                appName = appName.Replace("-", String.Empty);
+                if (dbContext.Applications.Any(item => item.Name.ToLower().Replace(" ", String.Empty) == appName))
+                {
+                    Application = dbContext.Applications.First(item => item.Name.ToLower().Replace(" ", String.Empty) == appName);
+                }
+                  
+            }
+     
         }
     }
 }
