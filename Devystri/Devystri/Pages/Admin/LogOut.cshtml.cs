536cb68ad7497dfc3e3659e87d238e725983590a
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-27 00:05:13 +0100

    link header

---
+++ Devystri/Devystri/Pages/Admin/LogOut.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Models.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Devystri.Pages.Admin
+{
+    public class LogOutModel : PageModel
+    {
+        public LogOutModel(SignInManager<AdminUser> signInManager)
+        {
+            signInManager.SignOutAsync();
+        }
+        public IActionResult OnGet()
+        {
+            return RedirectToPage("/Index");
+        }
+    }
+}
