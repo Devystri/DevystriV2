e01f58c49e64c749d6ef0c4d0014a518d42dbb46
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-26 20:49:58 +0100

    newsletter view

--- Devystri/Devystri/Pages/Admin/tableNewsletter.cshtml.cs
+++ Devystri/Devystri/Pages/Admin/tableNewsletter.cshtml.cs
@@ -2,15 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Devystri.Pages.Admin
 {
-    public class table_newsletterModel : PageModel
+    public class TableNewsletterModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public List<Newsletter> Newsletters { get; set; }
+        [BindProperty]
+        public int PageCount { get; set; }
+        [BindProperty]
+        public int ActualPage { get; set; }
+        public MyDbContext dbContext;
+        public TableNewsletterModel(MyDbContext context)
         {
+            dbContext = context;
+            
+        }
+        public void OnGet(int id = 1)
+        {
+            int numElPerPage = 15;
+            ActualPage = id;
+            PageCount = (dbContext.Newsletters.Count() / numElPerPage) + 1;
+            Newsletters = dbContext.Newsletters.Skip((id-1)* numElPerPage).Take(numElPerPage).ToList();
         }
     }
 }
