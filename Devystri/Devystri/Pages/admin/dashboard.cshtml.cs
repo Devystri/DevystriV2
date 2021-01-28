9b8871ccdb12b1d7960422cf8910d6ca3266bb96
DimitriTimoz <dimitri.timoz@orange.fr>
2021-01-29 00:15:47 +0100

    view newsletter and os

--- Devystri/Devystri/Pages/admin/dashboard.cshtml.cs
+++ Devystri/Devystri/Pages/admin/dashboard.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Data.Models.Statistics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,6 +16,18 @@ namespace Devystri.Pages.Admin
 #endif
     public class DashboardModel : PageModel
     {
+        [BindProperty]
+        public List<OSStats> oSStats { get; set; }
+        public List<NewsletterStats> NewsletterStats { get; set; }
+
+        private MyDbContext dbContext;
+        public DashboardModel(MyDbContext context)
+        {
+            dbContext = context;
+            oSStats = dbContext.OSs.ToList();
+            NewsletterStats = dbContext.NewsletterStats.ToList();
+
+        }
         public void OnGet()
         {
        
--- Devystri/Devystri/wwwroot/js/Pages/dashboard.js
+++ Devystri/Devystri/wwwroot/js/Pages/dashboard.js
@@ -1,57 +1,55 @@
-﻿var ctx = document.getElementById('visit-website-graphic').getContext('2d');
+﻿$(document).ready(() => {
+    var ctx = document.getElementById('visit-website-graphic').getContext('2d');
 
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
-        datasets: [{
-            label: 'Visites sur le site web',
-            backgroundColor: 'transparent',
-            borderColor: '#7CADD8',
-            data: [0, 10, 5, 2, 20, 30, 45, 200, 248 , 150 ,10 ,350],
-    }]
-    },
-    options: {}
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
+            datasets: [{
+                label: 'Visites sur le site web',
+                backgroundColor: 'transparent',
+                borderColor: '#7CADD8',
+                data: [0, 10, 5, 2, 20, 30, 45, 200, 248, 150, 10, 350],
+            }]
+        },
+        options: {}
+    });
+
+    var ctx = document.getElementById('popular-os-graphic').getContext('2d');
+
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: Os["labels"],
+            datasets: [{
+                data: Os["data"],
+                backgroundColor: [
+                    '#E17CFD',
+                    '#4CD7F6',
+                    '#6DBFFF',
+                    '#7166F9'
+                ],
+            }]
+        },
+
+        options: {}
+    });
+
+    var ctx = document.getElementById('newsletter-users-graphic').getContext('2d');
+
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: Newsletter["labels"],
+            datasets: [{
+                label: 'Inscrit à la newsletter',
+                backgroundColor: 'transparent',
+                borderColor: 'rgb(255, 190, 87)',
+                data: Newsletter["data"]
+            }]
+        },
+        options: {}
+    });
+
+
 });
-
-var ctx = document.getElementById('popular-os-graphic').getContext('2d');
-
-var myPieChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: [
-            'MacOS',
-            'Windows',
-            'IOS',
-            'Android'
-        ],
-        datasets: [{
-            data: [300, 50, 100, 200],
-            backgroundColor: [
-                '#E17CFD',
-                '#4CD7F6',
-                '#6DBFFF',
-                '#7166F9'
-            ],
-        }]
-    },
-
-    options: {}
-});
-
-var ctx = document.getElementById('newsletter-users-graphic').getContext('2d');
-
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
-        datasets: [{
-            label: 'Inscrit à la newsletter',
-            backgroundColor: 'transparent',
-            borderColor: 'rgb(255, 190, 87)',
-            data: [420000, 421243, 421863, 423594, 424856, 425123, 425486, 425542, 425795, 425142, 425764, 425053],
-        }]
-    },
-    options: {}
-});
-
