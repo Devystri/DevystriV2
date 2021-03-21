ff225852402838ec1f20be428139f5192bd27bf5
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2021-03-21 18:52:47 +0100

    AddProduct admin correction CSS and JS

--- Devystri/Devystri/wwwroot/js/Pages/admin/ImageImporter.js
+++ Devystri/Devystri/wwwroot/js/Pages/admin/ImageImporter.js
@@ -1,2 +1,59 @@
-﻿var el = $("input[type=file]");
-$(el).css("background", "url('"+el.attr("valu")+ "')");
+﻿
+
+// Function for logo image
+
+function previewLogo() {
+    var file = document.getElementById("fileLogo").files;
+    if (file.length > 0) {
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (event) {
+            document.getElementById("previewLogoAdd").setAttribute("src", event.target.result);
+        };
+
+        fileReader.readAsDataURL(file[0]);
+    }
+}
+
+// Function for "image de la page application"
+
+function previewImageApp() {
+    var file = document.getElementById("file").files;
+    if (file.length > 0) {
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (event) {
+            document.getElementById("previewApp").setAttribute("src", event.target.result);
+        };
+
+        fileReader.readAsDataURL(file[0]);
+    }
+}
+
+// Function for "Images qui entoure la fiche technique de l'application"
+
+function previewImage() {
+    var file = document.getElementById("file1").files;
+    if (file.length > 0) {
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (event) {
+            document.getElementById("preview").setAttribute("src", event.target.result);
+        };
+
+        fileReader.readAsDataURL(file[0]);
+    }
+}
+
+function previewImage2() {
+    var file = document.getElementById("file2").files;
+    if (file.length > 0) {
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (event) {
+            document.getElementById("preview2").setAttribute("src", event.target.result);
+        };
+
+        fileReader.readAsDataURL(file[0]);
+    }
+}
