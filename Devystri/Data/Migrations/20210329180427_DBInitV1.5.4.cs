29505e83da6940dd6c70bc544e27c668bb73613d
DimitriTimoz <dimitri.timoz@orange.fr>
2021-03-29 20:04:47 +0200

    migration

---
+++ Devystri/Data/Migrations/20210329180427_DBInitV1.5.4.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class DBInitV154 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PresentationRessource",
+                table: "Applications",
+                newName: "PresentationRessourceName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PresentationRessourceName",
+                table: "Applications",
+                newName: "PresentationRessource");
+        }
+    }
+}
