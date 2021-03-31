a97147301207464104e0af6d5dc2a021bca591b1
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2021-03-31 18:14:05 +0200

    add website load

--- Devystri/Devystri/Pages/Web-Sites/Index.cshtml.cs
+++ Devystri/Devystri/Pages/Web-Sites/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,37 @@ namespace Devystri.Pages.Web_Sites
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public WebSites webSite { get; set; }
+        public List<Section> Sections { get; set; }
+        private MyDbContext dbContext;
+
+        public IndexModel(MyDbContext context)
         {
+            dbContext = context;
+        }
+
+        public IActionResult OnGet(string appName)
+        {
+            if (appName == String.Empty)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                appName = appName.ToLower();
+                appName = appName.Replace("-", String.Empty);
+                var listSites = dbContext.WebSites.ToList();
+                if (listSites.Any(item => item.Name.ToLower().Replace(" ", String.Empty).Replace("?", String.Empty).Replace("&", String.Empty) == appName))
+                {
+                    webSite = listSites.First(item => item.Name.ToLower().Replace(" ", String.Empty).Replace("?", String.Empty).Replace("&", String.Empty) == appName);
+                    Sections = dbContext.Sections.Where(item => item.ProjectId == webSite.Id).ToList();
+                }
+
+
+            }
+            return Page();
+
         }
     }
 }
