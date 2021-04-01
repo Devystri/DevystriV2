338ca464c7927cc601b0b854a79b5dfe93fe01e7
DimitriTimoz <dimitri.timoz@orange.fr>
2021-04-01 13:44:19 +0200

    migration 1.5.8

--- Devystri/Data/Migrations/20210401114356_DBInitV1.5.8.Designer.cs
+++ Devystri/Data/Migrations/20210401114356_DBInitV1.5.8.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210401114356_DBInitV1.5.8")]
+    partial class DBInitV158
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Devystri/Data/Migrations/20210401114356_DBInitV1.5.8.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class DBInitV158 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PresentationRessource",
+                table: "Iots",
+                newName: "PresentationRessourceName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoName",
+                table: "Iots",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Presentation2RessourceName",
+                table: "Iots",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Presentation3RessourceName",
+                table: "Iots",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoName",
+                table: "Iots");
+
+            migrationBuilder.DropColumn(
+                name: "Presentation2RessourceName",
+                table: "Iots");
+
+            migrationBuilder.DropColumn(
+                name: "Presentation3RessourceName",
+                table: "Iots");
+
+            migrationBuilder.RenameColumn(
+                name: "PresentationRessourceName",
+                table: "Iots",
+                newName: "PresentationRessource");
+        }
+    }
+}
