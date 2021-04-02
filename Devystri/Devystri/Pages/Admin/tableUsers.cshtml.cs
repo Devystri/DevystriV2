31b7a70b32d092ce21c62fd09740a12946e121da
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2021-04-02 19:39:44 +0200

    get admin users

--- Devystri/Devystri/Pages/Admin/tableUsers.cshtml.cs
+++ Devystri/Devystri/Pages/Admin/tableUsers.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Data.Models.Entity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,36 @@ namespace Devystri.Pages.Admin
 {
     public class tableUsersModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public List<AdminUser> Users { get; set; }
+        [BindProperty]
+        public int PageCount { get; set; }
+        [BindProperty]
+        public int ActualPage { get; set; }
+        public IdentityAppContext dbContext;
+        public tableUsersModel(IdentityAppContext context)
         {
+            dbContext = context;
+
+        }
+        public void OnGet(int id = 1)
+        {
+            int numElPerPage = 15;
+            ActualPage = id;
+            PageCount = (dbContext.Users.Count() / numElPerPage) + 1;
+            Users = dbContext.Users.Skip((id - 1) * numElPerPage).Take(numElPerPage).ToList();
+        }
+        public IActionResult OnPost(int id)
+        {
+            var req = HttpContext.Request.Form;
+            if (dbContext.Users.Any(item => item.Id == id))
+            {
+                dbContext.Users.Remove(new AdminUser() { Id = id });
+                dbContext.SaveChanges();
+            }
+
+
+            return RedirectToPage("/Admin/tableUsers");
         }
     }
 }
